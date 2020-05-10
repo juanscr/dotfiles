e332b44f71544b06bc6b1041ceea059833f6af19
juanscr <jcardenasr123@gmail.com>
2020-05-10 06:48:56 -0500

    [ 201 ] Changed keybind for math mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,7 +58,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(LaTeX-math-abbrev-prefix "`")
+ '(LaTeX-math-abbrev-prefix "¬")
  '(inhibit-startup-screen t)
  '(package-selected-packages (quote (haskell-mode auctex))))
 
