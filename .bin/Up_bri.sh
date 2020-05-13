86fb5b16a9d980cb712bef1cd573d0475027bf57
juanscr <jcardenasr123@gmail.com>
2020-05-13 17:26:05 -0500

    Removed brightness scripts.

--- .bin/Up_bri.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bright=$(cat /sys/class/backlight/intel_backlight/brightness)
-max_bright=$(cat /sys/class/backlight/intel_backlight/max_brightness)
-step=$(($max_bright / 10 ))
-if [ "$bright" -lt "$max_bright" ]; then
-    bright=$(( $bright + $step))
-    echo "$bright" > /sys/class/backlight/intel_backlight/brightness
-fi
