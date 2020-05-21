9f99439ec28c56c5d3de85c37254e4bf515bfbdf
juanscr <jcardenasr123@gmail.com>
2020-05-20 20:16:26 -0500

    [ config ] Commented correctly.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -15,27 +15,5 @@
 ;; Show trailing whitespace
 (setq-default show-trailing-whitespace t)
 
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
+;;;;;;;;; ORG MODE COMMANDS ;;;;;;;;;
 (setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
-;; they are implemented.
