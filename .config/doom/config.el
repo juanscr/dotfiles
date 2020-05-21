b5642b2e3bb8deaa9d23b5cec1d27136e204348b
juanscr <jcardenasr123@gmail.com>
2020-05-20 20:31:37 -0500

    [ config ] Added math-mode and line numbers.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -10,10 +10,25 @@
 (setq doom-theme 'doom-dracula)
 
 ;; Show lines and columns
-(setq display-line-numbers-type t)
+(setq display-line-numbers t)
 
 ;; Show trailing whitespace
 (setq-default show-trailing-whitespace t)
+
+;;;;;;;;; MODES ;;;;;;;;;
+;; Spell check
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+;; Math mode
+; https://www.gnu.org/software/auctex/manual/auctex/Mathematics.html
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+
+;; Reftex mode
+; https://tex.stackexchange.com/questions/36876/reftex-doesnt-turn-on-automatically-when-loading-auctex-after-upgrade-to-tex-li
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+
 
 ;;;;;;;;; ORG MODE COMMANDS ;;;;;;;;;
 (setq org-directory "~/org/")
