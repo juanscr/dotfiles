24cfb61ea7ab1886fb210440190d4fd89e6b4cb6
juanscr <jcardenasr123@gmail.com>
2020-08-29 10:54:15 -0500

    [ polybar ] Corrected log creation.

--- .config/polybar/launch-bar.sh
+++ .config/polybar/launch-bar.sh
@@ -5,5 +5,5 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/polybar1.log
 polybar jscbar >>/tmp/polybar1.log 2>&1 &
