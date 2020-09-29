3dadc5e285a8f755fad9927150616d411c342d1b
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2020-09-29 16:03:20 -0500

    [ polybar ] Added multiple monitor launch.

--- .config/polybar/launch-bar.sh
+++ .config/polybar/launch-bar.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
+# Kill bar
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
-polybar jscbar >>/tmp/polybar1.log 2>&1 &
+# Launch bar in different monitors
+if type "xrandr"; then
+    monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
+    numMonitors=$(echo $monitors | wc -l)
+    echo $numMonitors
+    if [ $numMonitors == 1 ]; then
+        polybar --reload jscbar &
+    else
+        for m in $monitors; do
+            if [ $m == 'HDMI-0' ]; then
+                MONITOR=$m polybar --reload jscbar &
+            else
+                MONITOR=$m polybar --reload monitor2 &
+            fi
+        done
+    fi
+else
+    polybar --reload jscbar &
+fi
