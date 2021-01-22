3df804d7bca70004f17c5e00dee9eb65826f9525
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-01-22 11:58:13 -0500

    [ bin ] changed to personal build of dmenu, for homogeneity between calls

--- .bin/select-monitor.sh
+++ .bin/select-monitor.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-layout=$(find "$HOME"/.bin/monitor-layouts/ -type f -printf "%f\n"| dmenu -nf \
-         '#F8F8F2' -nb '#282A36' -sb '#6272A4' -sf '#F8F8F2' \
-         -fn 'DejaVu Sans-11' -p 'Select Layout:' )
+layout=$(find "$HOME"/.bin/monitor-layouts/ -type f -printf "%f\n"| \
+         dmenu -p 'Select Layout:' )
 
 "$HOME"/.bin/monitor-layouts/"$layout"
 i3-msg restart
