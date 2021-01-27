cb84c0fbbdf6c153083ded47b4fb449a1626215b
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-01-27 03:10:15 -0500

    [ bin ] remove command, worked buggy.

--- .bin/init-monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if type "xrandr"; then
-    monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
-    numMonitors=$(echo "$monitors" | wc -l)
-    if [ "$numMonitors" != 1 ]; then
-        "$HOME"/.bin/monitor-layouts/default.sh
-    fi
-fi
