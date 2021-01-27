566dd5095b4225352d8c41969ceb999259696631
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-01-27 02:57:02 -0500

    [ polybar ] simplified command.

--- .config/polybar/launch-bar.sh
+++ .config/polybar/launch-bar.sh
@@ -6,12 +6,12 @@ killall -q polybar
 # Launch bar in different monitors
 if type "xrandr"; then
     monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
-    numMonitors=$(echo $monitors | awk '{print NF}' | sort -nu | tail -n 1)
-    if [ $numMonitors == 1 ]; then
+    numMonitors=$(echo "$monitors" | wc -l)
+    if [ "$numMonitors" == 1 ]; then
         polybar --reload jscbar &
     else
         for m in $monitors; do
-            if [ $m == 'HDMI-0' ]; then
+            if [ "$m" == 'HDMI-0' ]; then
                 MONITOR=$m polybar --reload jscbar &
             else
                 MONITOR=$m polybar --reload monitor2 &
