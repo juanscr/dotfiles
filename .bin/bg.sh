c1efd8dcefeb37dfaaac7db386f915e24d628968
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-02-15 10:54:15 -0500

    [ bin ] added quotes for variables.

--- .bin/bg.sh
+++ .bin/bg.sh
@@ -2,5 +2,5 @@
 
 photo=$(find "$HOME"/pictures/wallpapers/ -name '*.jpg' | shuf -n 1)
 
-feh --no-fehbg --bg-scale $photo
-betterlockscreen -u $photo
+feh --no-fehbg --bg-scale "$photo"
+betterlockscreen -u "$photo"
