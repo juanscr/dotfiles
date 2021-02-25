b26132b300102b24d7d9b90a1ad24b29154b5306
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-02-25 16:37:44 -0500

    [ bin ] added optional input to select background.

--- .bin/bg.sh
+++ .bin/bg.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-photo=$(find "$HOME"/pictures/wallpapers/ -name '*.jpg' | shuf -n 1)
+if [ "$#" -lt 1 ]; then
+    photo=$(find "$HOME"/pictures/wallpapers/ -name '*.jpg' | shuf -n 1)
+else
+    photo=$1
+fi
 
 feh --no-fehbg --bg-scale "$photo"
 betterlockscreen -u "$photo"
