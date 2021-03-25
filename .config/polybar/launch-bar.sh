3f1bdda49c494236d920fbb69eea3fb5496151a1
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-03-25 18:34:55 -0500

    [ polybar ] removed redundant if, wayland not supported.

--- .config/polybar/launch-bar.sh
+++ .config/polybar/launch-bar.sh
@@ -4,20 +4,16 @@
 killall -q polybar
 
 # Launch bar in different monitors
-if type "xrandr"; then
-    monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
-    numMonitors=$(echo "$monitors" | wc -l)
-    if [ "$numMonitors" == 1 ]; then
-        polybar --reload jscbar &
-    else
-        for m in $monitors; do
-            if [ "$m" == 'HDMI-0' ]; then
-                MONITOR=$m polybar --reload jscbar &
-            else
-                MONITOR=$m polybar --reload monitor2 &
-            fi
-        done
-    fi
-else
+monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
+numMonitors=$(echo "$monitors" | wc -l)
+if [ "$numMonitors" == 1 ]; then
     polybar --reload jscbar &
+else
+    for m in $monitors; do
+        if [ "$m" == 'HDMI-0' ]; then
+            MONITOR=$m polybar --reload jscbar &
+        else
+            MONITOR=$m polybar --reload monitor2 &
+        fi
+    done
 fi
