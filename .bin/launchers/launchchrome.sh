9e812386a0aef5c048f3313e4e282768b2faefde
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-04-07 08:55:14 -0500

    [ bin ] added chrome launcher.

---
+++ .bin/launchers/launchchrome.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+clipboard=$(xsel --clipboard --output)
+
+# Regex for URLS
+regex1="[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]"
+regex="(https?|ftp|file)://$regex1"
+
+# Check if url
+if [[ "$clipboard" =~ $regex ]]; then
+    google-chrome-stable --new-window "$regex"
+else
+    google-chrome-stable
+fi
