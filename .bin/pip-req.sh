bfff0501c7e6c4dc0ef2991f97a95bc089257a51
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-04-09 14:49:39 -0500

    [ bin ] added command to get requirements for package.

---
+++ .bin/pip-req.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Get requirement
+getRequirement() {
+    # Unique lines
+    lines=$(echo "$1" | awk '!seen[$0]++')
+
+    # Get the requirement
+    requirements=""
+    freeze=$(pip freeze)
+    while IFS= read -r line; do
+        ignore="BEGIN{IGNORECASE=1}"
+        requirement=$(echo "$freeze" | awk "$ignore /^$line==/ { print \$0 }")
+        echo "$line"
+        echo "$requirement"
+
+        # Requirement
+        if [ "$requirement" != "" ]; then
+            if [ "$requirements" = "" ]; then
+                requirements="$requirement"
+            else
+                requirements="$requirements"$'\n'"$requirement"
+            fi
+        fi
+    done <<< "$lines"
+
+    echo "$requirements"
+}
+
+# Separate commands
+files=$(find "$1" -name '*.py' -type f -exec cat {} +)
+match="match(\$0, \"(import|from) [A-Za-z ,]+$\", a)"
+
+# Get imports
+imports=$(echo "$files" | awk "{ $match ; print a[0] }" | \
+          awk "!/^$/ { print \$2 }")
+
+# Get the requirements
+requirements=$(getRequirement "$imports")
+
+echo "$requirements" | awk "!/^$/ { print \$0 }"
