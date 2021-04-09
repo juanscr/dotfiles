e79726e07e06fdde95f8d4a00d5bcf75461ab6ce
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-04-09 09:58:54 -0500

    [ bin ] remove empty lines if necessary.

--- .bin/launchers/launchchrome.sh
+++ .bin/launchers/launchchrome.sh
@@ -4,6 +4,9 @@
 regex="match(\$0, \"(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+\", a)"
 url=$(xsel --clipboard --output | awk "{ $regex; print a[0] }")
 
+# Remove empty lines
+url=$(echo "$url" | awk '!/^$/')
+
 # Check if url
 if [ "$url" != "" ]; then
     google-chrome-stable --new-window "$url"
