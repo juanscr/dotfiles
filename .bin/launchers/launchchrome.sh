acc819f382e7a0afe7f073f8cead41f468895e89
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-04-09 09:55:47 -0500

    [ bin ] simplified match command.

--- .bin/launchers/launchchrome.sh
+++ .bin/launchers/launchchrome.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # URL extraction with awk
-regex="{ match(\$0, \"(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+\", a) }"
-url=$(xsel --clipboard --output | awk "$regex END { print a[0] }")
+regex="match(\$0, \"(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+\", a)"
+url=$(xsel --clipboard --output | awk "{ $regex; print a[0] }")
 
 # Check if url
 if [ "$url" != "" ]; then
