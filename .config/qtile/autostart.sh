7a5781f2b89ff80d928eea4110e0623834d077e4
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-07-10 15:27:48 -0500

    [ qtile ] start conky if only one monitor.

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -30,7 +30,12 @@ xrdb "$HOME"/.config/X11/Xresources &
 flameshot &
 
 # Conky
-conky &
+num_monitors=$("$HOME"/.config/qtile/check_number_of_monitors.sh)
+if [ "$num_monitors" == 2 ]; then
+    conky &
+else
+    conky --config="$HOME"/.config/conky/conky-one.conf &
+fi
 
 # Polkit
 polkit-dumb-agent &
