b930ae8dc38f02a8f7ff0c9f67865b6753539b44
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-07-10 15:36:34 -0500

    [ bin ] corrected if.

--- .bin/launchers/launch-terminal.sh
+++ .bin/launchers/launch-terminal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ `"$HOME"/.config/qtile/check_number_of_monitors.sh` == 1]; then
+if [ `"$HOME"/.config/qtile/check_number_of_monitors.sh` == 1 ]; then
     alacritty -o=font.size=9
 else
     alacritty
