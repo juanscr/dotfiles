2c1dcad880e6fbc59814daef2383215564f28fa3
Juan Sebastián Cárdenas <jcardenasr123@gmail.com>
2021-11-17 14:17:34 -0500

    [ ml ] always use HDMI as main monitor.

--- .bin/monitor-layouts/inverted.sh
+++ .bin/monitor-layouts/inverted.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal \
-       --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal \
+xrandr --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal \
+       --output HDMI1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal \
        --transform none
