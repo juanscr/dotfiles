f33e5dbc520a2a4d26e4f071125e864cb8d4967e
Juan Sebastián Cárdenas <jcardenasr123@gmail.com>
2021-11-17 15:47:51 -0500

    [ ml ] moved monitor down below.

--- .bin/monitor-layouts/default.sh
+++ .bin/monitor-layouts/default.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal \
+xrandr --output eDP1 --transform none --mode 1366x768 --pos 0x312 --rotate normal \
        --output HDMI1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal \
-       --transform none
