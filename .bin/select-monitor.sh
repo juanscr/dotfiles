5cd0a6fa6c5fbeb5fa6737a00adad29465cbb716
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2022-01-05 01:21:01 -0500

    [ bin ] do not check for number of monitors.

--- .bin/select-monitor.sh
+++ .bin/select-monitor.sh
@@ -7,14 +7,11 @@ if optimus-manager --print-mode | grep "integrated"; then
   EDP_NAME=eDP-1
 fi
 
-prev_num_monitors=$("$HOME/.config/qtile/check_number_of_monitors.sh")
 layout=$(find "$HOME"/.bin/monitor-layouts/ -type f -printf "%f\n" | \
          sort | \
          dmenu -p 'Select Layout:' )
 if [ "$layout" != "" ]; then
    "$HOME"/.bin/monitor-layouts/"$layout" "$EDP_NAME" "$HDMI_NAME"
-   num_monitors=$("$HOME/.config/qtile/check_number_of_monitors.sh")
-
    i3-msg restart || qtile cmd-obj -o cmd -f restart
    "$HOME"/.bin/launchers/launch-conky.sh &
 fi
