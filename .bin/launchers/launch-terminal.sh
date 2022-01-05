3c20214f99fa3fff829f16d4cf1165668ce6587c
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2022-01-05 02:12:49 -0500

    [ launchers ] corrected display detection.

--- .bin/launchers/launch-terminal.sh
+++ .bin/launchers/launch-terminal.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 script="$HOME"/.config/qtile/check_number_of_monitors.sh
-if [ `$script` == 1 ] && [ xrandr --listactivemonitors | grep -v "HDMI" ]; then
+if [ `$script` == 1 ] && xrandr --listactivemonitors | grep "eDP" ; then
     alacritty -o=font.size=9
 else
     alacritty
