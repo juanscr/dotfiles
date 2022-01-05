ec20389b81b149d810682c9d85aa0b36ac00af97
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2022-01-05 01:38:25 -0500

    [ launchers ] spawned conky according to layout.

--- .bin/launchers/launch-conky.sh
+++ .bin/launchers/launch-conky.sh
@@ -6,8 +6,16 @@ if optimus-manager --print-mode | grep "integrated"; then
     folder="$folder"/intel/
 fi
 num_monitors=$("$HOME"/.config/qtile/check_number_of_monitors.sh)
-if [ "$num_monitors" == 2 ]; then
-    conky --config="$folder"/conky.conf&
+coordinate=$(xrandr --query |
+             grep primary |
+             grep -o [[:digit:]]\\++[[:digit:]]\\+\\s |
+             awk -F+ '{ print $1 }')
+
+# Spawn conky according to layout
+if [ "$num_monitors" == 2 ] && [ "$coordinate" == 0 ]; then
+    conky --config="$folder"/conky.conf &
+elif [ "$num_monitors" == 2 ]; then
+    conky --config="$folder"/conky-left.conf &
 elif xrandr --listactivemonitors | grep "HDMI" ; then
     conky --config="$folder"/conky-one-hdmi.conf
 else
