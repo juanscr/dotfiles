6faec40023b7e45245ecba46576788be4ba0d75b
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2023-01-26 11:42:56 -0500

    [ nvim ] added dracula and telescope.

---
+++ .config/nvim/lua/juanscr/packages.lua
@@ -0,0 +1,17 @@
+-- Packer installation
+vim.cmd [[packadd packer.nvim]]
+
+-- Packages dependencies
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- Dracula theme
+  use 'Mofiqul/dracula.nvim'
+
+  -- Fuzzy finder
+  use {
+  	'nvim-telescope/telescope.nvim', 
+  	tag = '0.1.1',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+  }
+end)
