5edd4f430f19203428c7dd507197b35a4ee55ff9
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2023-02-03 11:38:50 -0500

    [ nvim ] improved copy-paste for system clipboard.

--- .config/nvim/lua/juanscr/keybinds.lua
+++ .config/nvim/lua/juanscr/keybinds.lua
@@ -8,5 +8,8 @@ vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Paste without replacing
+-- Copy and paste settings
 vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
