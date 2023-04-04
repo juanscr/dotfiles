ce587832b032469aa856639ede64173a1e9f4dda
Juan Sebastián Cárdenas <jcardenasr123@gmail.com>
2023-04-04 11:12:19 -0500

    [ nvim ] added neovim configuration.

---
+++ .config/nvim/lua/juanscr/packages.lua
@@ -0,0 +1,47 @@
+-- Packer installation
+vim.cmd [[packadd packer.nvim]]
+
+-- Packages dependencies
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- Dracula theme
+  use 'Mofiqul/dracula.nvim'
+
+  -- Fuzzy finder
+  use {
+  	'nvim-telescope/telescope.nvim',
+  	tag = '0.1.1',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- File project explore
+  use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+  }
+
+  -- LSP Configuration
+  use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v1.x',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},             -- Required
+          {'williamboman/mason.nvim'},           -- Optional
+          {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},         -- Required
+          {'hrsh7th/cmp-nvim-lsp'},     -- Required
+          {'hrsh7th/cmp-buffer'},       -- Optional
+          {'hrsh7th/cmp-path'},         -- Optional
+          {'saadparwaiz1/cmp_luasnip'}, -- Optional
+          {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},             -- Required
+          {'rafamadriz/friendly-snippets'}, -- Optional
+      }
+  }
+end)
