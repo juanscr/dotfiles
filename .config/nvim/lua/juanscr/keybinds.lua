f4c2332dd72bbe68b453c6e7bd80596d993d6f27
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2023-05-03 13:44:25 -0500

    [ nvim ] corrected clipboard behavior.

--- .config/nvim/lua/juanscr/keybinds.lua
+++ .config/nvim/lua/juanscr/keybinds.lua
@@ -6,7 +6,9 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Copy and paste settings
-vim.keymap.set("n", "<leader>cp", "\"*p")
+vim.keymap.set("n", "<leader>cp", "\"+p")
+vim.keymap.set("v", "<leader>cp", "\"+p")
+vim.keymap.set("n", "<leader>cP", "\"+P")
 vim.keymap.set("n", "<leader>cy", "\"+y")
 vim.keymap.set("v", "<leader>cy", "\"+y")
 vim.keymap.set("n", "<leader>cY", "\"+Y")
